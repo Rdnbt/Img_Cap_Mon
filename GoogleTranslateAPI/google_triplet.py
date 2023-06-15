2db45c1d29da232d698dc0fe450d4e9d5f190cf3
Harold Budd <haroldbudd12@gmail.com>
2023-06-15 14:29:34 +0900

    Created new Flickr1000 dataset and created triplet

--- GoogleTranslateAPI/google_triplet.py
+++ GoogleTranslateAPI/google_triplet.py
@@ -3,16 +3,20 @@ from googletrans import Translator
 
 def translate_text(text, dest_language):
     translator = Translator()
-    translation = translator.translate(text, dest=dest_language)
-    return translation.text
+    try:
+        translation = translator.translate(text, dest=dest_language)
+        return translation.text
+    except Exception as e:
+        print("Translation error:", e)
+        return ""
 
 def translate_csv(input_file, output_file, dest_language):
-    with open(input_file, 'r') as file:
+    with open(input_file, 'r', encoding='utf-8') as file:
         reader = csv.reader(file, delimiter='|')
         lines = list(reader)
         header = lines[0]  # Retrieve the header row
 
-    with open(output_file, 'w', newline='') as file:
+    with open(output_file, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter='|')
 
         # Write the header row to the output file
@@ -23,13 +27,13 @@ def translate_csv(input_file, output_file, dest_language):
             image_name, comment_number, comment = line
             translated_comment = translate_text(comment, dest_language)
 
-            # Append the translated Mongolian caption to the existing row
+            # Append the translated comment to the existing row
             line.append(translated_comment)
             writer.writerow(line)
 
 # Example usage
-input_file = 'test_short.csv'
-output_file = 'output_short.csv'
+input_file = 'output_1000.csv'
+output_file = 'triplet_1000.csv'
 destination_language = 'mn'  # Mongolian language code
 
 translate_csv(input_file, output_file, destination_language)
